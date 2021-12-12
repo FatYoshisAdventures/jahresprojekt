eade45b28c199af2594f6365388cf1add4dda1a0
iKirbs <anton.prem@hak-kitz.ac.at>
2021-12-12 15:59:44 +0100

    remove networking temp, cleanup player

--- jahresprojekt/Assets/Scripts/Movement.cs
+++ jahresprojekt/Assets/Scripts/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Rigidbody2D rb2d;
+    private Rigidbody2D rb;
     private float h = 0f;
     private float v = 0f;
     [SerializeField]
@@ -13,23 +13,22 @@ public class Movement : MonoBehaviour
 
     void Start()
     {
-        rb2d = this.GetComponent<Rigidbody2D>();
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
         h = Input.GetAxis("Horizontal");
-        //v = Input.GetAxis("Vertical");
 
-        transform.position += new Vector3(h * speed * Time.deltaTime, v * speed * Time.deltaTime, 0);
+        rb.velocity = new Vector2(h * speed * Time.fixedDeltaTime, rb.velocity.y);
 
-        Vector3 euler = transform.eulerAngles;
+        //Vector3 euler = transform.eulerAngles;
 
-        if (euler.z > 180) euler.z = euler.z - 360;
-        {
-            euler.z = Mathf.Clamp(euler.z, -25, 25);
-        }
+        //if (euler.z > 180) euler.z = euler.z - 360;
+        //{
+        //    euler.z = Mathf.Clamp(euler.z, -25, 25);
+        //}
 
-        transform.eulerAngles = euler;
+        //transform.eulerAngles = euler;
     }
 }
--- jahresprojekt/Assets/Scripts/TerrainCreator.cs
+++ jahresprojekt/Assets/Scripts/TerrainCreator.cs
@@ -10,34 +10,57 @@ public class TerrainCreator : MonoBehaviour
     private SpriteShapeController shape;
 
     [SerializeField]
-    private int numOfPoints = 10;
+    private int numOfPoints = 30;
 
     [SerializeField]
-    private float deviation = 2f;
+    private float height = 5f;
+
+    [SerializeField]
+    private float deviation = 5f;
+
+    [SerializeField]
+    private float mapSize = 30;
+
+    [SerializeField]
+    private float roundness = 2f;
 
     private void Start()
     {
-        shape.spline.SetPosition(0, shape.spline.GetPosition(0) - Vector3.right * 10);
-        shape.spline.SetPosition(1, shape.spline.GetPosition(1) - Vector3.right * 10);
-        shape.spline.SetPosition(2, shape.spline.GetPosition(2) + Vector3.right * 10);
-        shape.spline.SetPosition(3, shape.spline.GetPosition(3) + Vector3.right * 10);
+        ChangeMapSize();
 
+        GenerateTerrain();
+    }
+
+    private void GenerateTerrain()
+    {
+        // calculate exact horizontal size of map
         float scale = shape.spline.GetPosition(2).x - shape.spline.GetPosition(1).x;
 
+        // calculate what the distance between the points should be
         float distanceBtwnPoints = scale / numOfPoints;
 
-        for (int i = 0; i < numOfPoints-1; i++)
+        for (int i = 0; i < numOfPoints - 1; i++)
         {
             float xPos = shape.spline.GetPosition(i + 1).x + distanceBtwnPoints;
-            shape.spline.InsertPointAt(i + 2, new Vector3(xPos, Random.Range(0f, deviation)));
+            shape.spline.InsertPointAt(i + 2, new Vector3(xPos, deviation * Mathf.PerlinNoise(i * height * Random.Range(0f, 1f), 0)));;
         }
 
-        for (int i = 2; i < numOfPoints+1; i++)
+        for (int i = 2; i < numOfPoints + 1; i++)
         {
             shape.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
-            shape.spline.SetLeftTangent(i, new Vector3(-1, 0, 0));
-            shape.spline.SetRightTangent(i, new Vector3(1, 0, 0));
-
+            shape.spline.SetLeftTangent(i, new Vector3(-roundness, 0, 0));
+            shape.spline.SetRightTangent(i, new Vector3(+roundness, 0, 0));
         }
+    }
+
+    void ChangeMapSize()
+    {
+        //move points outwards to left
+        shape.spline.SetPosition(0, shape.spline.GetPosition(0) - Vector3.right * mapSize);
+        shape.spline.SetPosition(1, shape.spline.GetPosition(1) - Vector3.right * mapSize);
+
+        //move points outwards to right
+        shape.spline.SetPosition(2, shape.spline.GetPosition(2) + Vector3.right * mapSize);
+        shape.spline.SetPosition(3, shape.spline.GetPosition(3) + Vector3.right * mapSize);
     }
 }
