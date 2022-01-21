fa287157d806fb9a0aca4c727c39c7aba34fd3ac
ZHeYgen <ch.heigenhauser@hak-kitz.ac.at>
2022-01-21 08:30:04 +0100

    modifyed health

---
+++ jahresprojekt/Assets/Scripts/Health.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxhealth = 3;
+
+    [SerializeField]
+    private int _Health = 0;
+    [HideInInspector]
+    public int health 
+    {
+        get { return _Health; }
+        set {
+            if (value <= 0)
+            {
+                GameObject.Destroy(this.gameObject, 0);
+                //Instatniate corpse
+                if (gameObject.tag.Equals("Player"))
+                {
+                    //Finds animator on player death to kill player
+                    FindObjectOfType<Canvas>().GetComponent<Animator>().SetTrigger("Dead");
+                }
+            }
+            else
+            {
+                //sets health value between 0 and the maxamount of health
+                _Health = Mathf.Clamp(value, 0, maxhealth);
+            }
+        } 
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxhealth;
+    }
+}
